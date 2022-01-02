6b41b697d860b61de3318be65c06fd7554edf7fd
sunkhemrath <sunkhemrath309@gmail.com>
2022-01-02 22:19:34 +0700

    app smart_home

---
+++ MyApplication/app/src/main/java/com/example/myapplication/RegisterActivity.java
@@ -0,0 +1,72 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+    private EditText emailedit,passedit;
+    private Button btnregis;
+    private FirebaseAuth mAuth;
+    @Override
+    protected void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        mAuth=FirebaseAuth.getInstance();
+
+        emailedit=findViewById(R.id.email);
+        passedit=findViewById(R.id.password);
+        btnregis=findViewById(R.id.btnregis);
+
+
+        btnregis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                register();
+            }
+        });
+
+
+
+    }
+    private void register(){
+
+        String email,pass;
+            email=emailedit.getText().toString();
+            pass=passedit.getText().toString();
+
+            if (TextUtils.isEmpty(email)){
+                Toast.makeText(this,"vui long nhap email!!",Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (TextUtils.isEmpty(pass)){
+                Toast.makeText(this,"vui long nhap password!!",Toast.LENGTH_SHORT).show();
+                return;
+            }
+            mAuth.createUserWithEmailAndPassword(email,pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()){
+                        Toast.makeText(getApplicationContext(),"tao tai kjoan thanh cong",Toast.LENGTH_SHORT).show();
+                        Intent intent = new Intent(RegisterActivity.this,MainActivity.class);
+                        startActivity(intent);
+                    }else{
+                        Toast.makeText(getApplicationContext(),"Tao tai khoan khong thanh cong",Toast.LENGTH_SHORT).show();
+                }
+                }
+            });
+
+    }
+}
